5cd30ebcd1bad09d4dba73a0f3bf52011b3f3943
Кирилл Шахов <kirill.shakhov14@gmail.com>
2022-11-16 02:28:20 +0300

    feat(eureka): add config

--- src/main/java/ru/itmo/hps/lab1/core/CoreApplication.java
+++ src/main/java/ru/itmo/hps/lab1/core/CoreApplication.java
@@ -2,9 +2,11 @@ package ru.itmo.hps.lab1.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
