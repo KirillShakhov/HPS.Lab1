c599fe798e9896e2e35d5b869a2597674a11f1d8
Кирилл Шахов <kirill.shakhov14@gmail.com>
2022-11-15 21:57:27 +0300

    Initial commit

---
+++ src/test/java/ru/itmo/hps/lab1/attachment/AttachmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.hps.lab1.attachment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttachmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
