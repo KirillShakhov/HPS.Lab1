398fcc6b65aa0df662cab3d3b776163d8be734ed
Savva <savvasiry@gmail.com>
2022-12-01 00:50:46 +0300

    feat(Attachment): create Entities: Attachment.java, AttachmentType.java

---
+++ src/main/java/ru/itmo/hps/lab1/attachment/entity/Attachment.java
@@ -0,0 +1,34 @@
+package ru.itmo.hps.lab1.attachment.entity;
+
+
+import lombok.*;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "photos", schema = "public")
+public class Attachment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "base64", nullable = false)
+    private String base64;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @ToString.Exclude
+    private AttachmentType type;
+
+    @Column(name = "create_date", nullable = false)
+    private String createDate;
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/ru/itmo/hps/lab1/attachment/entity/AttachmentType.java
@@ -0,0 +1,7 @@
+package ru.itmo.hps.lab1.attachment.entity;
+
+public enum AttachmentType {
+    PHOTO,
+    AUDIO,
+    ICON,
+}
